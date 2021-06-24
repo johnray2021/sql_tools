18ca29f775e69c5c9b376fa8d2e9eb6c995bf56f
johnray2021 <85658720+johnray2021@users.noreply.github.com>
2021-06-24 11:28:21 +0800

    Create gen_password.sql

---
+++ gen_password.sql
@@ -0,0 +1,21 @@
+select replace(group_concat(substring(pwd_char,char_pos,1)),',','')
+  from (select '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*' as pwd_char,
+               1 as pos_id,round(rand()*52+10.5) as char_pos
+        union all
+        select *
+          from (select '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*' as pwd_char) t1, /*首字符为英文字母*/
+               (select 2 as pos_id,round(rand()*9+62.5) as char_pos union all    /*包含两个特殊字符*/
+                select 3 as pos_id,round(rand()*9+62.5) as char_pos union all    /*包含两个特殊字符*/
+                select 4 as pos_id,round(rand()*61+0.5) as char_pos union all   
+                select 5 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 6 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 7 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 8 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 9 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 10 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 11 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 12 as pos_id,round(rand()*61+0.5) as char_pos union all 
+                select 13 as pos_id,round(rand()*61+0.5) as char_pos             /*共生成13位长度的密码字符串*/
+                order by rand()                                                  /*随机排列特殊字符的位置顺序*/
+               ) t2
+        ) t3;
